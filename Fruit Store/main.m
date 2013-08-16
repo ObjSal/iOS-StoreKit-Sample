d339d30a413533d346dfcee1a7d5fd5bcdc50cc0
Salvador Guerrero <salvador.icc@gmail.com>
2013-08-15 20:38:35 -0700

    Initial Commit

---
+++ Fruit Store/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Fruit Store
+//
+//  Created by Salvador Guerrero on 8/15/13.
+//  Copyright (c) 2013 ByteApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
